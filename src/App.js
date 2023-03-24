2693b52d2a907de75facc1e8b37ff1b8d3cb1bc0
Sohaib Furqan <sfurqan@asaadshub.com>
2023-03-24 21:19:21 +0000

    Setup routes, folder structure, dependencies and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+import useStyles from './Components/styles';
+
+import {
+  Actors,
+  MovieInformation,
+  Movies,
+  NavBar,
+  Profile,
+} from './Components';
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolBar} />
+        <Switch>
+          <Route exact path={'/movie/:id'}>
+            <MovieInformation />
+          </Route>
+          <Route exact path={'/actors/:id'}>
+            <Actors />
+          </Route>
+          <Route exact path={'/'}>
+            <Movies />
+          </Route>
+          <Route exact path={'/profile/:id'}>
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
